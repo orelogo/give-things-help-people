d99415ee44944a52b5c212eda4d7fe88e4c8784e
orelogo <orelogo@gmail.com>
2017-03-11 13:41:41 -0800

    Add button components that produce alerts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './bootstrap.css';
 
 class App extends Component {
+  getItems(category) {
+    alert('You can get ' + category);
+  }
+  renderButton(name, category) {
+    return <CategoryButton name={name} onClick={() => this.getItems(category)} />;
+  }
   render() {
     return (
       <div className="App">
@@ -15,13 +22,21 @@ class App extends Component {
           Click on the type of donation you would like to give!
         </p>
         <p className="App-buttons">
-        <button onClick={() => alert("click")}>Clothing</button>
-        <button>Kitchen Items</button>
-        <button>Bedroom Items</button>
+        {this.renderButton("Clothing", "clothing")}
+        {this.renderButton("Kitchen Items", "kitchen")}
+        {this.renderButton("Bedroom Items", "bedroom")}
         </p>
       </div>
     );
   }
+}
+
+function CategoryButton(props) {
+  return (
+    <button className="btn btn-lg" onClick={() => props.onClick()}>
+      {props.name}
+    </button>
+  );
 }
 
 export default App;
