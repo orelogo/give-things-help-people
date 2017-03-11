001e3fde9ef9b1abf957958a8ba3c8540be59f29
orelogo <orelogo@gmail.com>
2017-03-11 14:17:21 -0800

    Have buttons, get errors

--- src/App.js
+++ src/App.js
@@ -2,13 +2,41 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './bootstrap.css';
+let jsonData = require('./items.json').items
 
 class App extends Component {
-  getItems(category) {
-    alert('You can get ' + category);
+  constructor() {
+    super();
+    this.state = {
+      listOfItems: null,
+    }
   }
+  renderItem(name, required, pledged) {
+    return <Item name={name} required={required} pledged={pledged} />;
+  }
+
+  getItems(category) {
+    let items = [];
+    for (let i = 0; i < jsonData.length; i++) {
+    	let item = jsonData[i];
+      if (item.category === category) {
+        items.push(item);
+      }
+    }
+    this.setState({
+      listOfItems: items
+    })
+  }
+
   renderButton(name, category) {
     return <CategoryButton name={name} onClick={() => this.getItems(category)} />;
+  }
+
+  renderItems() {
+    let items = this.state.listOfItems;
+    for (let i = 0; i < items.length; i++) {
+      renderItem(item.name, item.required, item.pledged);
+    }
   }
   render() {
     return (
@@ -22,10 +50,13 @@ class App extends Component {
           Click on the type of donation you would like to give!
         </p>
         <p className="App-buttons">
-        {this.renderButton("Clothing", "clothing")}
-        {this.renderButton("Kitchen Items", "kitchen")}
-        {this.renderButton("Bedroom Items", "bedroom")}
+          {this.renderButton("Clothing", "clothing")}
+          {this.renderButton("Kitchen Items", "kitchen")}
+          {this.renderButton("Bedroom Items", "bedroom")}
         </p>
+        <ul>
+          {renderItems()}
+        </ul>
       </div>
     );
   }
@@ -38,5 +69,14 @@ function CategoryButton(props) {
     </button>
   );
 }
+
+function Item(props) {
+  return (
+    <li>
+    {props.name}, Required: {props.required}, Pledged: {props.pledged}
+    </li>
+  );
+}
+
 
 export default App;
